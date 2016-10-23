773c200e4bd5dd614b47a8e6f4286349e1573380
Alejandro Alvarado <alexconh@hotmail.com>
2016-10-23 16:50:41 -0500

    Completed mail form

--- contact.php
+++ contact.php
@@ -6,23 +6,21 @@
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$message = $_POST['message'];
-	$formcontent="From: $name \n Message: $message";
-	$recipient = "alexconhg@gmail.com";
 	$subject = $_POST['subject'];
+	$formcontent="De: $name ($email) \nAsunto: $subject \nMensaje: \n$message";
 	$mailheader = "From: $email \r\n";
-	$mail_sent = mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+	$mail_sent = mail($mail_to, $subject, $formcontent, $mailheader) or die("Error!");
 	
 
 	if ($mail_sent == true){ ?>
 		<script language="javascript" type="text/javascript">
-			alert('Mensaje enviado');
-			window.location = 'contacto.html';
+			alert('Gracias por escribirnos! Tu mensaje fue enviado correctamente.');
+			window.location = 'contact.html';
 		</script>
 	<?php } else { ?>
 
 		<script type="text/javascript">
-			alert('Mensaje no enviado, consulta al administrador.');
-			window.location = 'contacto.html';
+			alert('Mensaje no enviado, intenta de nuevo o si el problema persiste consulta al administrador.');
 		</script>
 	<?php } 
 	?>
