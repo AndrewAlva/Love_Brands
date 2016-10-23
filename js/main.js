eff9a8153a0ab76ebd51be08562e59ae7bd1e360
Alejandro Alvarado <alexconh@hotmail.com>
2016-10-23 03:51:10 -0500

    General layout and style done, Pending to fix header, sliders and full navigation. Also mailing form pending

--- js/main.js
+++ js/main.js
@@ -1,13 +1,52 @@
+window.onload = function(){
+	Site.is_loading = false;
+	$('#loader').addClass('lb_crystal');
+	setTimeout(function(){$('#loader').addClass('lb_hide');},800);
+}
+
+
 $(document).ready(function() {
 	Site.init();
 });
 
+
+
 var initQuery = window.location.search;
 
 var Site = {
+	is_loading: true,
+
+	loading_dots: [],
+
 	init: function(){
 		if (initQuery == "") {
-			$('#yield').load('sections/contact.html');
+			$('#yield').load('sections/team.html');
+		};
+
+		Site.loading_dots = $('.isoDots');
+		Site.loading(0);
+	},
+
+	loading: function(dotCount){
+		var interval = 700;
+		if (Site.is_loading) {
+			if (dotCount < Site.loading_dots.length){
+				$('#dot-'+ dotCount).removeClass('active');
+				dotCount += 1;
+				$('#dot-'+ dotCount).addClass('active');
+
+
+				if(dotCount == Site.loading_dots.length){
+					dotCount = 0;
+					$('.isoDots').removeClass('active');
+					$('#dot-' + dotCount).addClass('active');
+				};
+
+				setTimeout(function(){
+					Site.loading(dotCount);
+				}, interval);
+
+			};
 		};
 	}
 }
